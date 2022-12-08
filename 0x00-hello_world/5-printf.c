0ce524c9e4dd106853acf899774a8d9336109183
joseph-chukwudiegwu <brainseph04@gmail.com>
2022-12-08 09:33:04 -0800

    correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - using printf to print in c program
+ * main - using a printf to print a c program
  * Return: 0
 */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
