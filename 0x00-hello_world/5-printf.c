ffa09acd36805b9b5cb119b2d741c7c32b0f766c
joseph-chukwudiegwu <brainseph04@gmail.com>
2022-12-08 08:40:42 -0800

    Task 5.Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - using printf to print in c program
+ * Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art.\n");
+	return (0);
+}
