d6fa5fe96237717f88b50c9ff8949c23d2f0f4a9
joseph-chukwudiegwu <brainseph04@gmail.com>
2022-12-08 10:09:46 -0800

    Task 7 & 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.>
+/**
+ * main - Entry
+ * Return: 1
+*/
+int main(void)
+{
+	write(1, "and thad piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
