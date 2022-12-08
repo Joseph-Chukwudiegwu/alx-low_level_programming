d50a33aa18590673c9cde4c2a12bdd00f0ddf4e8
joseph-chukwudiegwu <brainseph04@gmail.com>
2022-12-08 15:39:02 -0800

    correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdout>
+#include <unistd.h>
 /**
  * main - Entry
  * Return: 1
@@ -8,3 +8,4 @@ int main(void)
 {
 	write(2, "and thad piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
+}
