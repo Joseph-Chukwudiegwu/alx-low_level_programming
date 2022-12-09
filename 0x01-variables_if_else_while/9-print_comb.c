1f7f0f70ce4bb8c2a3c0af3376dfc498304fe277
joseph-chukwudiegwu <brainseph04@gmail.com>
2022-12-09 14:56:57 -0800

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,10 +7,10 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 10; num++)
+	for (num = 48; num < 58; num++)
 	{
 		putchar(num);
-		if (num != 8)
+		if (num != 57)
 		{
 			putchar(',');
 			putchar(' ');
