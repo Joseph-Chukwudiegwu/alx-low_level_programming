c84701eb2a4eabb35605893d5ffaa7345cf89054
joseph-chukwudiegwu <brainseph04@gmail.com>
2022-12-09 16:57:57 -0800

    Task 10 & 11

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,4 +5,27 @@
 */
 int main(void)
 {
+	int x, y;
 
+	x = 48;
+	y = 48;
+
+	while (x < 58)
+	{
+		y = x + 1;
+		while (y < 58)
+		{
+			putchar(x);
+			putchar(y);
+			if (x < 56 || y < 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			y++;
+		}
+		x++;
+	}
+	putchar(10);
+	return (0);
+}
--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,3 +5,35 @@
 */
 int main(void)
 {
+	int x, y, z;
+
+	x = 48;
+	y = 48;
+	z = 48;
+
+	while (x < 58)
+	{
+		y = x + 1;
+		while (y < 58)
+		{
+			z = y + 1;
+			while (z < 58)
+			{
+				putchar(x);
+				putchar(y);
+				putchar(z);
+
+				if (x < 55 || y < 56 || z < 57)
+				{
+					putchar(44);
+					putchar(32);
+				}
+				z++;
+			}
+			y++;
+		}
+		x++;
+	}
+	putchar(10);
+	return (0);
+}
