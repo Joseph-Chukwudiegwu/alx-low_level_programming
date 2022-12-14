64014e708cec9aa2b5d0b3ca3b5324fcdc7bba98
joseph-chukwudiegwu <brainseph04@gmail.com>
2022-12-13 18:26:16 -0800

    correction

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,6 +10,7 @@ int main(void)
 	int num;
 	unsigned long fib1 = 0, fib2 = 1, sum;
 	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
+	unsigned long half1, half2;
 
 	for (num = 0; num < 92; num++)
 	{
