bdee4fc4cec5ab8e1b57ffe5394029716ccb0437
joseph-chukwudiegwu <brainseph04@gmail.com>
2022-12-19 13:06:01 -0800

    correction

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,14 +8,16 @@
 
 int _atoi(char *s)
 {
+	int c = 0;
+	unsigned int ni = 0;
 	int min = 1;
 	int isi = 0;
-i
+
 	while (s[c])
 	{
 		if (s[c] == 45)
 		{
-			min += -1;
+			min *= -1;
 		}
 
 		while (s[c] >= 48 && s[c] <= 57)
